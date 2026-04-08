c71ad40f94f5f76c949c6e8c4fe2c758d3a90f0e
nydialane <nydia.lane@mymail.champlain.edu>
2026-04-08 09:47:10 -0400

    playbook

--- 480driver.ps1
+++ 480driver.ps1
@@ -1,8 +1,8 @@
 
 # import module
-import-module "/home/nydia/SEC-480/SEC-480/modules/480-utils/480-utils.psm1" -force
+import-module "/home/nydia/SEC-480/modules/480-utils/480-utils.psm1" -force
 #path to json file
-$configpath = "/home/nydia/SEC-480/SEC-480/480.json"
+$configpath = "/home/nydia/SEC-480/480.json"
 
 Clear-Host
 480banner
